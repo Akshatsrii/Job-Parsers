14c549c4834c6afdf3735c694e36d545fe16a814
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-07-11 12:36:47 +0530

    Inject FloatingChatbot globally in MainLayout.jsx layout wrapper

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import Navbar from "../components/common/Navbar.jsx";
 import Footer from "../components/common/Footer.jsx";
+import FloatingChatbot from "../components/common/FloatingChatbot.jsx";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="flex min-h-screen flex-col bg-gray-50">
+    <div className="flex min-h-screen flex-col bg-[#f8fafc]">
       <Navbar />
-      <main className="mx-auto w-full max-w-6xl flex-1 px-4 py-8">{children}</main>
+      <main className="mx-auto w-full max-w-6xl flex-1 px-6 py-8">{children}</main>
       <Footer />
+      <FloatingChatbot />
     </div>
   );
 }
+
